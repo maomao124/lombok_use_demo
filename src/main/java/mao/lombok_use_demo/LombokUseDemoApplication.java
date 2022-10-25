4c9435599e4d4245416987454148bbe781be6290
mao <1296193245@qq.com>
2022-10-25 22:43:36 +0800

    lombok_use_demo

---
+++ src/main/java/mao/lombok_use_demo/LombokUseDemoApplication.java
@@ -0,0 +1,15 @@
+package mao.lombok_use_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LombokUseDemoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(LombokUseDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mao/lombok_use_demo/LombokUseDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.lombok_use_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LombokUseDemoApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
